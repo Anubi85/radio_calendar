a5749d5504bd9b6c481e4ccec2c6ca54836c08a6
Andrea <andrea.parisotto.85@gmail.com>
2021-03-02 20:49:48 +0100

    Remove unused enums

--- Enums.py
+++ Enums.py
@@ -4,15 +4,6 @@ class StrEnum(str, Enum):
     def __new__(cls, value):
         return str.__new__(cls, value)
 
-class Input(StrEnum):
-    PowerState = 'power-state'
-    Play = 'play-pressed'
-    Stop = 'stop-pressed'
-    Next = 'next-pressed'
-    Prev = 'previous-pressed'
-    VolUp = 'volume-up-pressed'
-    VolDown = 'volume-down-pressed'
-
 class StationTag(StrEnum):
     Position = 'position'
     DisplayName = 'display-name'
