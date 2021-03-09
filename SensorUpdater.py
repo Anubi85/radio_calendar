d0376eb9014b5ce6cb4e958f58883af9bc8aeb68
Andrea <andrea.parisotto.85@gmail.com>
2021-03-09 23:34:11 +0100

    Fix bug in sensor compensation

--- SensorUpdater.py
+++ SensorUpdater.py
@@ -9,7 +9,7 @@ class SensorUpdater:
         self.__i2c_bus = smbus2.SMBus(1)
         self.__bme280 = bme280.BME280(i2c_dev=self.__i2c_bus)
         self.__compensation_data = compensation_data
-        self.__cpu_temperature_buffer = [0]*15
+        self.__cpu_temperature_buffer = []
         self.__cpu_temperature = 0
         #first time we need to update the sensor, wait a little bit and then update again
         #otherwise the sensor produces uncorrect values
@@ -19,14 +19,15 @@ class SensorUpdater:
     def update(self):
         try:
             self.__logger.debug('Updating BME280 data')
-            self.__cpu_temperature_buffer.pop(0)
             cpu_temp = self.__get_cpu_temperature()
             if cpu_temp:
                 self.__cpu_temperature_buffer.append(cpu_temp)
             else:
                 self.__logger.warning('CPU temperature not updated, keep using last value')
                 self.__cpu_temperature_buffer.append(self.__cpu_temperature_buffer[-1])
-            self.__cpu_temperature = sum(self.__cpu_temperature_buffer)/len(self.__cpu_temperature_buffer)
+            if len(self.__cpu_temperature_buffer) > 15:
+                self.__cpu_temperature_buffer.pop(0)
+                self.__cpu_temperature = sum(self.__cpu_temperature_buffer)/len(self.__cpu_temperature_buffer)
             self.__bme280.update_sensor()
         except Exception as ex:
             #some error occured, log the exception and keep trying
